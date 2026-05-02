6f1b0de7d553f3e6351e9fdb92c9f9f78ba19235
Rafael JP Damaceno <rafael@pitangainnovare.com.br>
2026-05-01 22:34:06 -0300

    feat: registrar novos apps em INSTALLED_APPS, menu e wagtail hooks

--- config/menu.py
+++ config/menu.py
@@ -1,13 +1,10 @@
 WAGTAIL_MENU_APPS_ORDER = {
-    "collection": 100,
-    "article": 200,
-    "journal": 300,
-    "resources": 400,
-    "log_manager": 500,
-    "log_manager_config": 600,
-    "metrics": 700,
-    "tasks": 800,
-    "unexpected-error": 900,
+    "metadata": 100,
+    "resources": 200,
+    "log_manager": 300,
+    "tracker": 400,
+    "metrics": 500,
+    "tasks": 600,
 }
 
 def get_menu_order(app_name):
